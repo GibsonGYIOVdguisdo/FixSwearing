6da6411c1ad73ba46a6b45a7cc64b1e1a940f6c9
Gibson <Gibson570@hotmail.com>
2024-12-28 17:25:40 +0000

    Completed logic

--- main.py
+++ main.py
@@ -7,27 +7,34 @@ def get_banned_words():
             banned_words.append(word.strip())
     return banned_words
 
-def contains_bad_words(string):
+def contains_bad_words(text):
     banned_words = get_banned_words()
-    words = string.split()
+    words = text.split()
     for word in words:
         for banned_word in banned_words:
             if word.lower() == banned_word.lower():
                 return True
     return False
 
+def bad_word_found():
+    print("run stuff")
+
 def main():
     r = sr.Recognizer() # Create recogniser instance
     mic = sr.Microphone() # Create microphone instance
+    print("began listening")
     while True: # Main loo
         with mic as source:
+            r.adjust_for_ambient_noise(source)
             audio = r.listen(source)
         try:
             text = r.recognize_google(audio)
         except:
             text = ""
-        print(text)
-        print(contains_bad_words(text))
+        if text != "" and contains_bad_words(text):
+            bad_word_found()
+        else:
+            print(text)
     # r.recognize_sphinx(audio) works offline but is less accurate
     # r.recognize_google(audio) only works online but is very accurate
 
